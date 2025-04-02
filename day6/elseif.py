19eaa211310c76924f3b6d77ae20e07c7da251ea
Michael Diogo <89931817+DiogoMic@users.noreply.github.com>
2025-04-02 20:14:45 +0000

    added more elif

---
+++ day6/elseif.py
@@ -0,0 +1,8 @@
+print("SECURE LOGIN")
+username = input("Username > ")
+if username == "mark":
+  print("Welcome Mark!")
+elif username == "Diogo":
+  print("Welcome Diogo!")
+else:
+  print("Go away!")
