7ae4aa31962b687da3c0878e89948b966574751c
Michael Diogo <89931817+DiogoMic@users.noreply.github.com>
2025-04-03 21:40:42 +0000

    day7 I learnt about nested if statement

---
+++ day7/nested.py
@@ -0,0 +1,12 @@
+tvShow = input("What is your favorite tv show? ")
+if tvShow == "vikings":
+  print("Ugh, why?")
+  faveCharacter = input("Who is your favorite character? ")
+  if faveCharacter == "ragnarok":
+    print("Right answer")
+  else:
+    print("Nah, ragnarol is the greatest")
+elif tvShow == "game of thrones":
+  print("Aww, sad times")
+else:
+  print("Yeah, that's cool and all…")
